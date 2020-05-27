dac6e708a7ae3c71df6eb9b2a5b24a1617d5c452
Anthony Xu <57514814+axu8888@users.noreply.github.com>
2020-05-27 00:37:10 -0500

    Add files via upload

--- Command/Command.cpp
+++ Command/Command.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
-#include <stdlib.h>
 #include "Command.h"
+#include "../Database/Database.h"
+#include "../Student/Student.h"
+#define COMP(comm) command.compare(comm) == 0
 
 using namespace std;
 
@@ -12,12 +14,38 @@ using namespace std;
 void Command::get_command(string command)
 {
     //Exits the program
-    if(command.compare("exit") == 0)
+    if(COMP("exit"))
     {
         Command::exit_prog();
         return;
     }
-        //If the given command does not match, then notify user
+    else if(COMP("list"))
+    {
+        Command::list_commands();
+        return;
+    }
+    else if(COMP("search"))
+    {
+        Database::search_db();
+        return;
+    }
+    else if(COMP("save"))
+    {
+         Database::save_data();
+    }
+    else if(COMP("load"))
+    {
+         Database::load_data();
+    }
+    else if(COMP("ANS"))
+    {
+        
+    }
+    
+    
+    
+    
+    //If the given command does not match, then notify user
     else
     {
         cout << "Command not recognized.\n";
@@ -25,10 +53,55 @@ void Command::get_command(string command)
 }
 
 /*
-
+ Destructs and exits the program
+ Input: None
+ Output: None
 */
-void Command::exit_prog()
+void Command::exit_prog(Student * student)
 {
     //call all destructors needed
-    exit(0);
+    Database::save_data();
+    delete student;  //deletes student on heap
+    exit(0); 
 }
+
+
+void Command::list_commands()
+{
+    cout << "search -> searches database for class of a specified attribute\n";
+    cout << "exit -> saves data and closes program" << "\n";
+    cout << "save -> saves data\n" ; 
+    cout << "load -> loads data\n" ;
+    cout << "ANS -> add new student, WARNING : overwrites current student data \n";
+}
+
+void Command::firstAdd(Student * student){
+    cout << "Add courses, to leave type 'exit'\n";
+    while(true) {
+        string exit;
+        student->addCourse();
+        if(cin >> exit)
+        {
+            if(exit.compare("exit")==0)
+            {
+                return;
+            } else {
+                cout << "Invalid input";
+            }
+
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
--- Command/Command.h
+++ Command/Command.h
@@ -1,9 +1,11 @@
 #pragma once
 #include <string>
-
+#include <Student.h>
 class Command
 {
 public:
     static void get_command(std::string command);
-    static void exit_prog();
+    static void exit_prog(Student * student);
+    static void list_commands();
+    static void firstAdd(Student * student);
 };
