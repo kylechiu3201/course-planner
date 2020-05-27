60b7e1cd141213bba94bf76276fdb07fa322cd9f
Anthony Xu <57514814+axu8888@users.noreply.github.com>
2020-05-27 00:40:53 -0500

    Add files via upload

--- Engineering/Engineering.cpp
+++ Engineering/Engineering.cpp
@@ -3,8 +3,47 @@
 #include "Engineering.h"
 #include "../Course/Course.h"
 using namespace std;
+
+const int Engineering::tot_hours = 128;
 vector<Course> Engineering::get_geneds()
+{
+    return pos_geneds;
+}
+
+int Engineering::get_tot_hours()
+{
+    return tot_hours;
+}
+
+ Engineering * Engineering::choose_major(string maj){
+    if(maj.compare("CE") == 0)
     {
-        return geneds;
+        Engineering * obj = new CE();
+    }
+    else if(maj.compare("CS")==0)
+    {
+        Engineering * obj = new CS();
+    }
+    else if(maj.compare("EE")==0)
+    {
+        Engineering * obj = new EE();
+    }
+    else{
+        cout << "Invalid Major";
+        return NULL;
+    }
+    return &obj;
+}
+
+    bool Engineering::search_req(vector<Course> vec, Course c)
+    {
+        for(int x= 0 ; x< vec.size() ; x++)
+        {
+            if(vec[x].get_major().compareto(c.get_major())==0 && vec[x].get_course_num() == c.get_course_num())
+            {
+                return true;
+            }
+        }
+        return false;
     }
     
--- Engineering/Engineering.h
+++ Engineering/Engineering.h
@@ -3,18 +3,46 @@
 #include <vector>
 #include "../Course/Course.h"
 
-//Class contains general courses overlapping between ECE and CS 
-class Engineering{   //make class abstract??? Do we need to create this?
-    protected:
+//Class contains general courses overlapping between ECE and CS
+class Engineering{   //ABSTRACT CLASS
+protected:
+    static const int tot_hours;
+    //interacts with Student Class
+    //lib ed requirements
+        //gened categories
+   static vector <Course> pos_geneds;
+   static vector <Course> pos_lang;
+   static vector <Course> pos_compos;
+   
+   
+   //geneds
+    vector <Course> west_comp = vector<Course>(2);
+    vector <Course> non_west = vector<Course>(2);
+    vector <Course> US_min = vector<Course>(2);
+    vector <Course> hum_and_arts = vector<Course>(2);
+    vector <Course> soc_and_behav = vector<Course>(2);
+    vector <Course> libed = vector<Course>(2);
     
-    static const int req_hours; //interacts with Student Class
-    vector <Course> geneds;
-    vector <Course> language;
-    vector <Course> physics; //211 and 212
-    vector <Course>adv_comp;
-    vector <Course> misc; //ENG 100
+    //3 language courses
+    vector <Course> language = vector<Course>(3);
+    //Composition 1 and Advanced Composition
+    vector <Course> compos = vector<Course>(2);
+    vector <Course> misc = vector<Course>(1); //ENG 100 + whatever extra bs you need
+   
+
+public:
+    //constructors
+    Engineering()
     
-    public:
-    vector<Course> get_geneds();
+    //getters
+    
+    //other functions
+    
+    static vector<Course> get_geneds();
+    static int get_tot_hours();
+    static Engineering * choose_major(string maj);
+    virtual void add_to_degree(Course c, Engineering * major) = 0;  //pure virtual function to make class virtual
+    static bool search_req(vector <Course> vec); //helper function for add_to_degree
+    virtual void fill_pos_vec() = 0;
     
 };
