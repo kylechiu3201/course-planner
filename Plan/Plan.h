69010752d749c2088eb8574ea8c2bd0a6694e97b
Anthony Xu <57514814+axu8888@users.noreply.github.com>
2020-08-09 21:30:45 -0500

    Add files via upload

---
+++ Plan/Plan.h
@@ -0,0 +1,30 @@
+#pragma once
+
+//8 semesters + up to 4 summer/winter semesters
+//min 12 max 18 for school year, except for last semesters//max 8?? for summer
+//semester - contains four courses
+
+
+class Plan {
+    
+    private:
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public:
+    
+    
+    
+    
+    
+    
+    
+    
+}
