b844ebe9df30819463260cc3395c6258b5b24ed1
Anthony Xu <57514814+axu8888@users.noreply.github.com>
2020-08-10 22:40:47 -0500

    Add files via upload

--- Student/Student.cpp
+++ Student/Student.cpp
@@ -7,15 +7,16 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include "../Semester/Semester.h"
 
 using namespace std;
 //constructors
-Student::Student(/*vector<Semester> my_plan, */vector<Course> & cour, int syear, int gyear, Engineering * maj, int tcred, int deg, string n){
-    //plan = my_plan;
+Student::Student(vector<Semester> my_plan, vector<Course> & cour, int syear, int gyear, Engineering * maj, int tcred, int deg, string n){
+    plan = my_plan;
     cour_taken = cour;
     start_year = syear;
     grad_year = gyear;
-    //change this to Major object instead of string
+   //change this to Major object instead of string
     stud_major = maj;
     GPA = calculateGPA(cour);
     tot_credit = tcred;
@@ -24,7 +25,7 @@ Student::Student(/*vector<Semester> my_plan, */vector<Course> & cour, int syear,
 }
 
 Student::Student(Engineering * maj, string n, int syear){
-    //plan = vector<Semester>();
+    plan = {vector<Semester>(16, Semester())};
     cour_taken = vector<Course>();
     start_year = syear;
     grad_year = 0;
@@ -33,11 +34,11 @@ Student::Student(Engineering * maj, string n, int syear){
     GPA = 0;
     tot_credit = 0;
     degree_cred = 0;
-
+    
 }
 
 Student::Student(){
-    //plan = vector<Semester>();
+    plan = vector<Semester>(16, Semester());
     cour_taken = vector<Course>();
     start_year = 0;
     grad_year = 0;
@@ -101,11 +102,13 @@ string Student::get_name(){
     return name;
 }
 
-/*
 Semester Student::get_sem(int num){
     return plan[num-1];
 }
- */
+
+vector<Semester> Student::get_sem_vec(){
+    return plan;
+}
 
 //setters
 void Student::set_start_year(int year){
@@ -138,18 +141,24 @@ void Student::addCourse(){
     int class_num;
     string course;
     string grade;
+    
+    //user picks semester to add course to
+    cout << "Which semester would you like to add the course to?\n";
+    display_sem_names(plan);
+    int index = pick_semester(plan);
 
+    //retrieve input for course
     cout << "Add Course, Format: 'Major_CourseNum'" << endl;
     cin >> course >> class_num;
-
+    
     //add function to find corresponding course in database
-    Course c = Database::find_course(course, class_num);
-    if(c.get_name()=="")  //c is pointer lul
+    Course c = Database::find_course(course, class_num); //find_course returns Course() object
+    if(c.get_name()=="") 
     {
-        cout << "Course not found. Type 'add' to try again\n";
+        cout << "Course not found. Type 'add' to try again\n"; //NEED TO ADD OPTION TO ADD COURSE TO DATABASE
         return;
     }
-
+    
     /*for(int i = 0; i<cour_taken.size(); i++){   //checks if course is already taken
         if(cour_taken[i].get_major().compare(c->get_major())==0 && cour_taken[i].get_course_num()==c->get_course_num()){ //compare courses
             cout << "Course already taken. Type 'add' to try again\n";
@@ -170,113 +179,187 @@ void Student::addCourse(){
         cout << "\nInvalid grade. Type 'add' to try again";
         return;
     }
-
-    if(CompE::check_degree(c))
+    if(plan[index].valid_sem(c)) //checks if adding course goes over 18 hours
     {
-        degree_cred += c.get_credit_hours();
-        stud_major->add_to_degree(c);
+        if(CompE::check_degree(c))
+        {
+            degree_cred += c.get_credit_hours();
+            stud_major->add_to_degree(c);  //adds course to degree
+        }
+        cour_taken.push_back(c); //adds course to general student course vector
+        plan[index].get_sem_vec().push_back(c); //adds course to correct semester
+        
+        plan[index].add_hours(c.get_credit_hours()); //QUESTION:: should i go  get_plan()[index]....instead?
+        //does ...plan[index].get_hours()++ work
+        tot_credit += c.get_credit_hours(); 
+        GPA = calculateGPA(cour_taken);
     }
-    cour_taken.push_back(c);
-    tot_credit += c.get_credit_hours(); //we need to define if course is degree cred
-    GPA = calculateGPA(cour_taken);
-}
+} 
 
 void Student::removeCourse(){
+    
+    cout << "Pick semester to remove from";
+    int index = pick_semester(plan);
+    
     string course;
     int class_num;
     cout << "Remove Course, Format: 'Major_CourseNum'" << endl;
     cin >> course >> class_num ;
-
+    
     Course c = Course(course, class_num);
-
+    
     for(int i = 0; i<cour_taken.size(); i++){
         //if(cour_taken[i]==c){ //compare courses using overloaded operator
         if(cour_taken.at(i).get_major().compare(c.get_major())==0 && cour_taken.at(i).get_course_num()==c.get_course_num())
         {
-            if(stud_major->remove_from_degree(c)){
+            if(stud_major->remove_from_degree(c)){  //removes from degree
                 degree_cred -= c.get_credit_hours();
             }
-            tot_credit -= cour_taken[i].get_credit_hours(); //We need to define if course is degree cred
-            cour_taken.erase(find(cour_taken.begin(), cour_taken.end(), c));
+            tot_credit -= cour_taken[i].get_credit_hours(); 
+            cour_taken.erase(find(cour_taken.begin(), cour_taken.end(), c));  //remove from general course vector
+            plan[index].get_sem_vec().erase(find(plan[index].get_sem_vec().begin(), plan[index].get_sem_vec().end(),c)); //remove from semester
             GPA = calculateGPA(cour_taken);
             return;
         }
     }
     cout << "Course not found";
-
+    
 }
-
-
-Student * Student::create_new_student(string & name)
-{
+ 
+ 
+ Student * Student::create_new_student(string & name)
+ {
     string user_name;
     int start_year;
-
+    
     std::cout << "Type your major: ";
-
+    
     std::cin >> name ;
 
     Engineering * m = Engineering::choose_major(name);
     while(m == NULL)
     {
         std::cout << "Type your major: ";
+        cin >> name;
         m = Engineering::choose_major(name);
     }
     cout << "Type your name: ";
     cin >> user_name ;
     cout << "Type enrollment year: ";
     cin >> start_year ;
-
+    
     Student * student = new Student(m, user_name, start_year);
+    
+    return student;  
+ }
+ 
+ bool Student::valid_grade(string g){ //check to see if string is a valid grade
+     if(g.compare("A+") == 0){
+            return true;
+        }
+        if(g.compare("A") == 0){
+            return true;
+        }
+        if(g.compare("A-") == 0){
+            return true;
+        }
+        if(g.compare("B+") == 0){
+            return true;
+        }
+        if(g.compare("B") == 0){
+            return true;
+        }
+        if(g.compare("B-") == 0){
+            return true;
+        }
+        if(g.compare("C+") == 0){
+            return true;
+        }
+        if(g.compare("C") == 0){
+            return true;
+        }
+        if(g.compare("C-") == 0){
+            return true;
+        }
+        if(g.compare("D+") == 0){
+            return true;
+        }
+        if(g.compare("D") == 0){
+            return true;
+        }
+        if(g.compare("D-") == 0){
+            return true;
+        }
+        if(g.compare("F") == 0){
+            return true;
+        }
+        if(g.compare("PS") == 0){
+            return true;
+        }
+        if(g.compare("\n") == 0){
+            return true;
+        }
+        return false;
+    }
+    //Displays all semester names
+    void Student::display_sem_names(vector<Semester> vec)  
+    {
+        for(int x=0 ; x< vec.size() ; x++)
+        {
+            cout << vec[x].get_sem_name()  << "\n";
+        }
+    }
+    
+    
+    
+    int Student::pick_semester(vector<Semester> vec)
+    {
+        string input;
+        getline(cin, input);
+        int index = 0;
+        while(input != vec[index].get_sem_name() && index < vec.size())
+        {
+            index++;
+        }
+        if(index == vec.size())
+        {
+            cout << "Invalid Semester. Type 'add' to try again";
+        }
+        
+        return index;
+    }
 
-    return student;
-}
-
-bool Student::valid_grade(string g){ //check to see if string is a valid grade
-    if(g.compare("A+") == 0){
-        return true;
+void Student::stud_sem_name(Student *student) //sets semester names according to start year
+{
+    int year = student->get_start_year();
+    string set = "";
+    for(int x = 3 ; x< student->get_sem_vec().size()+2 ; x++)
+    {
+        if(x % 4 == 1)
+        {
+            set = "Spring";
+            set+= to_string(year);
+            student->get_sem(x-2).set_sem_name(set);
+        }
+        if(x % 4 == 2)
+        {
+            set = "Summer";
+            set+= to_string(year);
+            student->get_sem(x-2).set_sem_name(set);
+        }
+        if(x % 4 ==3)
+        {
+            set = "Fall";
+            set+= to_string(year);
+            student->get_sem(x-2).set_sem_name(set);
+        }
+        if(x % 4 ==0)
+        {
+            set = "Winter";
+            set+= to_string(year);
+            student->get_sem(x-2).set_sem_name(set);
+            year++; //update year every 4 semesters
+        }
+        set = "";
     }
-    if(g.compare("A") == 0){
-        return true;
-    }
-    if(g.compare("A-") == 0){
-        return true;
-    }
-    if(g.compare("B+") == 0){
-        return true;
-    }
-    if(g.compare("B") == 0){
-        return true;
-    }
-    if(g.compare("B-") == 0){
-        return true;
-    }
-    if(g.compare("C+") == 0){
-        return true;
-    }
-    if(g.compare("C") == 0){
-        return true;
-    }
-    if(g.compare("C-") == 0){
-        return true;
-    }
-    if(g.compare("D+") == 0){
-        return true;
-    }
-    if(g.compare("D") == 0){
-        return true;
-    }
-    if(g.compare("D-") == 0){
-        return true;
-    }
-    if(g.compare("F") == 0){
-        return true;
-    }
-    if(g.compare("PS") == 0){
-        return true;
-    }
-    if(g.compare("\n") == 0){
-        return true;
-    }
-    return false;
 }
--- Student/Student.h
+++ Student/Student.h
@@ -4,13 +4,14 @@
 #include <iostream>
 #include "../Course/Course.h"
 #include "../Engineering/Engineering.h"
+#include "../Semester/Semester.h"
 
 using namespace std;
 
 class Student {
 private:
     vector<Course> cour_taken;
-    //vector<Semester> plan;
+    vector<Semester> plan;
     int start_year;
     int grad_year;
     Engineering * stud_major; //Engineering class?
@@ -21,17 +22,17 @@ private:
 
 public:
     //constructor
-    Student(vector<Course> & cour, int syear, int gyear, Engineering* maj, int tcred, int deg, string n);
+    Student (vector<Semester> my_plan, vector<Course> & cour, int syear, int gyear, Engineering* maj, int tcred, int deg, string n);
     Student(Engineering* maj, string n, int syear);
     Student();
 
     //getters
     double calculateGPA(vector<Course> & cour);
-
+    
     vector<Course> get_cour_taken();
 
     int get_start_year();
-
+    
     int get_grad_year();
 
     Engineering * get_stud_major();
@@ -41,32 +42,39 @@ public:
     int get_tot_credit();
 
     int get_degree_cred();
-
+    
     string get_name();
-
-    //Semester get_sem(int num);
-
+    
+    Semester get_sem(int num);
+    vector<Semester> get_sem_vec();
+    
     //setters
     void set_start_year(int year);
-
+    
     void set_grad_year(int year);
 
     void set_stud_major(Engineering *  maj);
 
     void set_tot_credit(int tcred);
-
+    
     void set_degree_cred(int deg);
-
+    
     void set_name(string n);
 
     //Other Functions
     void addCourse();
 
     void removeCourse();
-
+    
     static Student* create_new_student(string & name);
-
+    
     int get_cred_needed();
-
+    
     bool valid_grade(string g);
+    
+    static void display_sem_names(vector<Semester> vec);
+    
+    int pick_semester(vector<Semester> vec); //returns index of semester
+
+    void stud_sem_name(Student *student);
 };
